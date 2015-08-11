01018f11a4e446ed89d150ec4813805483a283ee
Alexa Zeazas & Rachael Berecka <student@epicodus.com>
2015-08-11 10:04:38 -0700

    Correct typos.

--- spec/task_integration_spec.rb
+++ spec/task_integration_spec.rb
@@ -4,11 +4,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
-describe('the task path', {:type => :feature} do
+describe('the task path', {:type => :feature}) do
   it('process the user entry and adds it to the task list') do
     visit('/')
     fill_in('description', :with => 'watch Doctor Who')
     click_button('Add task')
-    expect(page).to have_content('The task has been successfully submitted')
+    expect(page).to have_content('The task has been successfully submitted!')
   end
 end
